6969eb59bcef58f58d7ce49c8a42c2957475ef33
Davida Marion <davida.marion@gsa.gov>
2021-12-02 14:44:49 -0500

    Day 1 solved

--- 2021/day1/depth.rb
+++ 2021/day1/depth.rb
@@ -1,17 +1,18 @@
 require "pry"
 
 class Depth
-  attr_reader :depth_list
+  attr_reader :basic_list, :simple
   attr_accessor :increases
 
-  def initialize(path:)
-    @depth_list = File.read(path).split("\n").map(&:to_i)
+  def initialize(path:, simple:)
+    @basic_list = File.read(path).split("\n").map(&:to_i)
+    @simple = simple
     @increases = 0
     find_increases
   end
 
-  def self.run(path: "./2021/day1/input.txt")
-    new(path: path).run
+  def self.run(path: "./2021/day1/input.txt", simple: true)
+    new(path: path, simple: simple).run
   end
 
   def run
@@ -24,6 +25,21 @@ class Depth
       
       @increases = @increases + 1 if depth > depth_list[idx-1]
     end
+  end
+
+  def triples
+    @basic_list.each_with_index.map {|depth, idx| [depth, @basic_list[idx+1], @basic_list[idx+2]].compact}
+      .select {|triple| triple.count == 3}
+  end
+
+  def triples_sum_list
+    triples.map(&:sum)
+  end
+
+  def depth_list
+    return @basic_list if @simple
+
+    triples_sum_list
   end
 
 end
--- spec/2021/day1/depth_spec.rb
+++ spec/2021/day1/depth_spec.rb
@@ -1,22 +1,60 @@
 require './2021/day1/depth'
 
 RSpec.describe Depth do
-
+  test_path = "./spec/2021/day1/input.txt"
+  
   describe "run" do
     it "outputs a number" do
-      expect(Depth.run(path: "./spec/2021/day1/input.txt")).to be_an(Integer)
+      expect(Depth.run(path: test_path)).to be_an(Integer)
     end
 
+    describe "simple method" do
+      describe "with test input" do
+        it "returns 7" do
+          expect(Depth.run(path: test_path)).to eq 7
+        end
+      end
+
+      describe "with real input" do
+        it "returns the real answer" do
+          expect(Depth.run).to eq 1616
+        end
+      end 
+    end
+
+    describe "complex method" do
+      describe "with test input" do
+        it "returns 5" do
+          expect(Depth.run(path: test_path, simple: false)).to eq 5
+        end
+      end
+
+      describe "with real input" do
+        it "returns the real answer" do
+          expect(Depth.run(simple: false)).to eq 1645
+        end
+      end 
+    end
+  end
+
+  describe "triples" do
     describe "with test input" do
-      it "returns 7" do
-        expect(Depth.run(path: "./spec/2021/day1/input.txt")).to eq 7
+      let(:depth) { Depth.new(path: test_path, simple: false) }
+
+      it "returns the correct triples" do
+        triples = [[199,200,208], [200,208,210], [208,210,200], [210,200,207], [200,207,240], [207,240,269], [240,269,260], [269,260,263]]
+
+        expect(depth.triples).to eq triples
       end
     end
+  end
 
-    describe "with real input" do
-      it "returns the real answer" do
-        expect(Depth.run).to eq 1616
-      end
-    end 
+  describe "triples_sum_list" do
+    let(:depth) { Depth.new(path: test_path, simple: false) }
+    it "returns an array of the sums of the triples correctly" do
+      sums = [607,618,618,617,647,716,769,792]
+
+      expect(depth.triples_sum_list).to eq sums
+    end
   end
 end
